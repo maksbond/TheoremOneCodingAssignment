f7e34c8c5586c211a3f11567d145ec027e2bae67
maksbond <bondarmaksymwork@gmail.com>
2023-01-07 13:06:19 -0500

    Added initial files for Networking. Updated Xcode files structure

---
+++ TheoremOneCodingAssignment/Model/Network/Networking.swift
@@ -0,0 +1,13 @@
+//
+//  Networking.swift
+//  TheoremOneCodingAssignment
+//
+//  Created by Maksym Bondar on 07.01.2023.
+//
+
+protocol Networking {
+    func fetchPosts() async throws -> [Post]
+    func fetchUser(for post: Post) async throws -> User
+    func fetchComments(for post: Post) async throws -> [Comment]
+    func deletePost(with id: UInt64) async throws
+}
