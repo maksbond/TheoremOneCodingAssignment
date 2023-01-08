17892119df7e6307d1246ecfc967dc972dfac345
maksbond <bondarmaksymwork@gmail.com>
2023-01-08 14:37:00 -0500

    Added data source to fetch and present posts

--- TheoremOneCodingAssignment/SceneDelegate.swift
+++ TheoremOneCodingAssignment/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = PostsTableViewController(networkManager: NetworkManager())
+            window.rootViewController = UINavigationController(rootViewController: PostsTableViewController(networkManager: NetworkManager()))
             self.window = window
             window.makeKeyAndVisible()
         }
